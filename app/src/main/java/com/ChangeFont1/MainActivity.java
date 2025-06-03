b055b8c4adf196480f1748557cacad85e37e2df3
Ahmed fawzey <afgjava851@gmail.com>
2025-06-03 11:57:20 +0300

    Update MainActivity.java

--- app/src/main/java/com/ChangeFont1/MainActivity.java
+++ app/src/main/java/com/ChangeFont1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ChangeFont1;
+  package com.ChangeFont1;
 
 import android.app.*;
 import android.os.*;
@@ -17,73 +17,45 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		 
+
 }
-@Override
-public boolean onCreateOptionsMenu(Menu menu){
-	menu.add("معلومات").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-	menu.add("احمد").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-	return true;
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 1, Menu.NONE, "معلومات")
+            .setIcon(R.drawable.info)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
 
-@Override 
-public boolean onOptionsItemSelected(final MenuItem item) {
-	switch (item.getTitle().toString()) {
-		case "معلومات":
-			 
-			/*Intent i = new Intent( android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
-			  startActivity( i );*/
-			// Check if app is whitelisted
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-				PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-				if (!pm.isIgnoringBatteryOptimizations(getPackageName())) {
-					// If app is not whitelisted, display settings screen to add it to whitelist
-					Toast.makeText(this, "تحتاج إلى إضافة التطبيق إلى القائمة البيضاء لضمان عمله في الخلفية", Toast.LENGTH_LONG).show();
-					Intent intent = new Intent();
-					intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-					intent.setData(Uri.parse("package:" + getPackageName()));
-					startActivity(intent);
-				} else {
-					// App is already whitelisted
-					Toast.makeText(this, "تمت إضافة التطبيق إلى القائمة البيضاء", Toast.LENGTH_SHORT).show();
-				}}
-			
-		return true;
-		case "احمد":
-			Toast.makeText(this,item.getTitle().toString(),-1).show();
-			/*Intent intent = new Intent();
+        menu.add(Menu.NONE, 2, Menu.NONE, "الاعدادت")
+            .setIcon(R.drawable.setting)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
 
-			int sdkVersion = android.os.Build.VERSION.SDK_INT;
-			String settingsAction;
+        // استخدام الانعكاس لإظهار الأيقونات
+        try {
+            Class<?> menuBuilderClass = Class.forName("com.android.internal.view.menu.MenuBuilder");
+            if (menuBuilderClass.isInstance(menu)) {
+                java.lang.reflect.Method m = menuBuilderClass.getDeclaredMethod(
+                    "setOptionalIconsVisible", boolean.class);
+                m.setAccessible(true);
+                m.invoke(menu, true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			if (sdkVersion >= 26) {
-				settingsAction = android.provider.Settings.ACTION_APP_NOTIFICATION_SETTINGS;
-				intent.setAction(settingsAction);
-				intent.putExtra(android.provider.Settings.EXTRA_APP_PACKAGE, getPackageName());
-			} else if (sdkVersion >= 21) {
-				settingsAction = "android.settings.APP_NOTIFICATION_SETTINGS";
-				intent.setAction(settingsAction);
-				intent.putExtra("app_package", getPackageName());
-				intent.putExtra("app_uid", getApplicationInfo().uid);
-			} else {
-				settingsAction = android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
-				intent.setAction(settingsAction);
-				intent.addCategory(Intent.CATEGORY_DEFAULT);
-				Uri uri = Uri.fromParts("package", getPackageName(), null);
-				intent.setData(uri);
-			}
-
-			startActivity(intent);*/
-			Intent intent = new Intent();
-			String settingsAction = "android.settings.APP_NOTIFICATION_SETTINGS";
-			intent.setAction(settingsAction);
-			intent.putExtra("app_package", getPackageName());
-			intent.putExtra("app_uid", getApplicationInfo().uid);
-			startActivity(intent);
-		return true;
-		default:
-		return super.onOptionsItemSelected(item);
-	}
+        return true;
+    }
+    @Override 
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getTitle().toString()) {
+            case "معلومات":
+                Toast.makeText(getApplicationContext(), "معلومات", Toast.LENGTH_LONG).show();
+                return true;
+            case "الاعدادت":
+                Toast.makeText(getApplicationContext(),"الاعدادت",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 }
 public void Aaa(View v){
 	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this,0);
